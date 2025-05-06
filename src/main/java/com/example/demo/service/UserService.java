fe0ad35aadff327050a255b9833176cd45d43db0
subodhfyntune <nagarkar.subodh@fyntune.com>
2025-05-06 14:50:30 +0530

    password encryption decryption done

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.config.JwtUtil;
 import com.example.demo.model.Users;
 import com.example.demo.repository.UserRepository;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,27 +14,41 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, JwtUtil jwtUtil) {
+    public UserService(UserRepository userRepository, JwtUtil jwtUtil ) {
         this.userRepository = userRepository;
         this.jwtUtil = jwtUtil;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public String registerUser(Users user) {
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             return "Username already exists";
         }
+        String hashedPassword =  passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         userRepository.save(user);
         return "User registered successfully";
     }
 
     public String loginUser(Users user) {
         Optional<Users> existingUser = userRepository.findByUsername(user.getUsername());
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
-        	Users userlogin = existingUser.get();
-            return jwtUtil.generateToken(userlogin.getUsername(), userlogin.getUserId() ,userlogin.getEmail(),userlogin.getRole());
+
+        if (existingUser.isPresent()) {
+            Users userlogin = existingUser.get();
+         
+            if (passwordEncoder.matches(user.getPassword(), userlogin.getPassword())) {
+                return jwtUtil.generateToken(
+                    userlogin.getUsername(),
+                    userlogin.getUserId(),
+                    userlogin.getEmail(),
+                    userlogin.getRole()
+                );
+            }
         }
         return "Invalid username or password";
     }
+
 
 }
